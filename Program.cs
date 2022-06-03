cc41da1671070ac0c18991662fa97bea853d8f74
Breno Marcomini <brenomarcomini@hotmail.com>
2022-06-02 22:18:42 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+//builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+app.UseRouting();
+app.MapControllerRoute(
+    name:"padrao",
+    //pattern:"{controller=Home}/{action=Index}/{id?}",
+    pattern: "{controller}/{action}/{id?}",
+    defaults: new {Controller = "Home", Action = "Index" }
+);
+
+app.Run();
+
+//app.MapGet("/", () => "Olá Vitat");
+//app.MapGet("/admin", (string username) => "Olá Admin Vitat" + username);
